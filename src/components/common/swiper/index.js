49e24f08b8865c175fab6cdb74699200196173ff
Berry <zhangxu755@@hotmail.com>
2021-02-18 15:38:02 +0800

    轮播图Swiper引入。首页轮播图页面提交

---
+++ src/components/common/swiper/index.js
@@ -0,0 +1,7 @@
+import Swiper from './Swiper'
+import SwiperItem from './SwiperItem'
+
+// 将轮播图组件统一导出，在使用的时候导入可以更方便
+export {
+  Swiper, SwiperItem
+}
---
+++ src/views/home/childComps/HomeSwiper.vue
@@ -0,0 +1,37 @@
+/* 首页的轮播图 */
+<template>
+  <div>    
+    <swiper>
+      <swiper-item v-for="item in banners" :key="item.index">
+        <a :href="item.link">
+          <img :src="item.image">
+        </a>
+      </swiper-item>
+    </swiper>
+  </div>
+</template>
+
+<script>
+import {Swiper, SwiperItem} from "components/common/swiper"
+
+export default {
+  name:"HomeSwiper",
+  components: {
+    Swiper,
+    SwiperItem
+  },
+  props:{
+    banners:{
+      type: Array,
+      required: true,
+      default(){
+        return []
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
